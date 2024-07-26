3ede0e5e41119995d4ce37ea0097ae13750c75bb
cob118 <maincore449@hotmail.com>
2024-07-26 13:20:42 +0900

    d

---
+++ Assets/P_Assets/P-Scripts/Follow_Cam.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow_Cam : MonoBehaviour
+{
+
+    GameObject target; // 카메라가 비출 타켓 
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+       // transform.position - target.transform.position; // 타켓ㄱㅘ ㅋ
+    }
+}
