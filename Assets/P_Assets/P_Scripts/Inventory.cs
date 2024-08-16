6ce653e86fde35effa68921c6c6d13ec579d16d3
cob1887 <maincore449@hotmail.com>
2024-08-16 20:03:37 +0900

    Ui 이미지 스프라이트 만듬

--- Assets/P_Assets/P_Scripts/Inventory.cs
+++ Assets/P_Assets/P_Scripts/Inventory.cs
@@ -1,18 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class Inventory : MonoBehaviour
 {
     public GameObject[] inventory;
+    public Image[] uiSprite = new Image[4]; //     ITME의 스프라이트 이미지를 여기에 저장
+   
+
+
     private int selectedIndex = -1;
 
     public void Start()
     {     
       
-       inventory = new GameObject[4]; 
-            
+       inventory = new GameObject[4];
+        
+
+       
         
     }
 
@@ -29,6 +37,7 @@ public class Inventory : MonoBehaviour
             if (inventory[i] == null)
             {
                 inventory[i] = item; // 비어있는 칸에 아이템 저장
+                UpdateSprite(i);
 
                 break;
             }
@@ -47,6 +56,7 @@ public class Inventory : MonoBehaviour
         if (index >= 0 && index < inventory.Length)
         {
             selectedIndex = index; // 선택된 인덱스를 selectedindex 에 저장
+            UpdateSprite(index);
             return inventory[index];
         }
         return null;
@@ -61,7 +71,77 @@ public class Inventory : MonoBehaviour
         if (index >= 0 && index < inventory.Length)
         {
             inventory[index] = null; // 배열에서 해당 아이템을 null로 설정
+            UpdateSprite(index);
         }
+    }
+
+
+
+    // 아이템 스프라이트를 업데이트하는 함수
+    public void UpdateSprite(int index)
+    {
+        for(int i = 0; i < uiSprite.Length; i++)
+        {
+            // 인벤토리 배열이 비어있지 않다면
+            if (inventory[i] != null)
+            {
+                // 배열 안에 있는 게임 오브젝트의 item 컴포넌트를 가져온다.
+                Item item = inventory[i].GetComponent<Item>();  
+                
+                if(item != null)
+                {
+                    uiSprite[i].sprite = item.itemSprite;
+                    uiSprite[i].gameObject.SetActive(true);
+                }
+                else // 아이템칸이 비어있다면
+                {
+                    Debug.Log("아이템칸이 비어있습니다.");
+
+                    uiSprite[i].sprite = null;
+                    uiSprite[i].gameObject.SetActive(false);
+
+                }
+            }
+            else // 인벤토리 배열이 비어있다면 // 필요한가이게?
+            {
+                uiSprite[i].sprite = null;
+                uiSprite[i].gameObject.SetActive(false);
+            }
+        }
+
+
+
+
+
+
+
+
+
+        //if (index >= 0 && index < inventory.Length)
+        //{
+        //    if (inventory[index] != null && uiSprite[index] != null)
+        //    {
+        //        Item item = inventory[index].GetComponent<Item>();
+
+        //        if(item != null)
+        //        {
+        //            uiSprite[index].sprite = item.itemSprite; // 아이템이 비어있지 않다면 아이템의 스프라이트가 uispirte 의 sprite 이다
+        //        }
+        //        else
+        //        {
+        //            Debug.Log(" item 배열이 비어있습니다.");
+
+        //            uiSprite[index].sprite = null;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        if (uiSprite[index] != null)
+        //        {
+        //            uiSprite[index].sprite = null; // 빈 슬롯의 경우 스프라이트 제거
+        //        }
+        //    }
+        //}
     }
 
 
